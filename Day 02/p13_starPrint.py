ef59233cf7eeaa02d6d2a427a06072a2cb92724e
vinca0224 <vinca0224@naver.com>
2024-02-22 15:55:50 +0900

    되새김 문제 풀이

--- Day 02/p13_starPrint.py
+++ Day 02/p13_starPrint.py
@@ -1,3 +1,26 @@
 # file : p13_starPrint.py
 # desc : 별 찍기
 
+print('예제 그림')
+for i in range(1 , 5+1):
+    print('*' * i)
+
+# 2중 for문으로 결과를 똑같이 만드시오
+print('\n')
+
+print('별찍기 ----------->> ')
+
+for i in range(1,6): # 손 댈 필요 없음
+    for j in range(i, 6):
+        print(' ', end='')
+
+    for j in range(6-i, 6):
+        print('*', end='')
+    print()
+
+print('별찍기 ----------->> ')
+
+for i in range(1,6): # 손 댈 필요 없음
+    for j in range(i):
+        print('*', end='')
+    print()
---
+++ Day 02/p14_review2.py
@@ -0,0 +1,35 @@
+# file : p14_review2.py
+# desc : 되새김 문제
+
+# Q1. 3의 배수의 합 구하기
+result = 0
+i = 1
+while i <= 1000:
+    if i % 3 == 0:
+        result += i
+    i += 1
+print(result)
+
+# Q2. 별 표시하기
+i = 0
+while True:
+    i += 1
+    if i > 5 : break
+    print('*' * i)
+
+# Q3. 1부터 100까지 출력하기
+for i in range(1,101):
+    print(i)
+
+# Q4. 평균 점수 구하기
+A = [70,60,55,75,95,90,80,80,85,100]
+total = 0
+for score in A:
+    total += score
+average = total / len(A)
+print(average)
+
+# Q5. 리스트 컴프리헨션 사용하기
+numbers = [1,2,3,4,5]
+result = [num*2 for num in numbers if num % 2 ==1]
+print(result)
