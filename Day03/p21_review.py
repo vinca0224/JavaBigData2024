a524f7b4104e3716e9c81bd6b78332318781e24e
vinca0224 <vinca0224@naver.com>
2024-02-23 14:12:17 +0900

    퀴즈 풀이

--- Day03/p21_review.py
+++ Day03/p21_review.py
@@ -2,10 +2,20 @@
 # desc : 되새김 문제
 
 # Q1. 파일의 문자열 바꾸기
-f = open('./Day03/test.txt', 'r')
-body = f.read()
+## read() 사용
+# f = open('./Day03/test.txt', 'r', encoding='utf-8')
+# body = f.read() # 문자열로 리턴, 그러나 read()는 텍스트가 길면 문장이 잘려서 나온다.(전부 출력할수없다) 
+# f.close()
+# body = body.replace('java', 'python')
+# f = open('./Day03/test.txt', 'w', encoding='utf-8')
+# f.write(body)
+# f.close()
+
+## readlines() 사용
+f = open('./Day03/test.txt', 'r', encoding='utf-8')
+body = f.readlines() # 리스트로 리턴
 f.close()
-body = body.replace('java', 'python')
-f = open('./Day03/test.txt', 'w')
-f.write(body)
+body = [body[0],body[1].replace('java', 'python')]
+f = open('./Day03/test.txt', 'w', encoding='utf-8')
+f.write(body[0] + body[1])
 f.close()
