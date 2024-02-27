a6cc9b8a18827fb82f42efce3c95d008083bad39
vinca0224 <vinca0224@naver.com>
2024-02-27 11:17:41 +0900

    주소록 수정

--- Day05/p35_addrBook.py
+++ Day05/p35_addrBook.py
@@ -1,4 +1,4 @@
-# file : p34_unicode.py
+# file : p35_addrBook.py
 # desc : 콘솔 주소록 프로그램
 
 class Contact: # 주소록 클래스
@@ -15,14 +15,33 @@ class Contact: # 주소록 클래스
                f'이메일 : {self.__eMail__}\n'
                f'주  소 : {self.__addr__}')
         return res
+def setContact(): # 사용자 입력으로 주소록 받기
+    (name, phoneNumber, eMail, addr) = input('주소록 입력(이름, 핸드폰, 이메일, 주소[구분자 /]) > ').split('/')
+    name = name.strip() # 사용자 실수로 들어간 스페이스 제거
+    phoneNumber = phoneNumber.strip()
+    eMail = eMail.strip()
+    addr = addr.strip()
+    print(f'"{name}","{phoneNumber}","{eMail}","{addr}"')
+
+def displayMenu():
+    menu =('주소록 프로그램\n'
+           '1. 연락처 추가\n'
+           '2. 연락처 출력\n'
+           '3. 연락처 삭제\n'
+           '4. 종료\n')
+    print(menu)
+    sel = int(input('메뉴입력 : '))
+    return sel
 
 def run():
-    # first = Contact(addr='경성', phoneNumber='010-1234-5678', name='홍길동', eMail='sdf@naver.com')
-    first = Contact('홍길동', '010-1234-5678', 'seee@naver.com', '경성')
-    print(first)
+    while True:
+        selMenu = displayMenu()
+        if selMenu == 4:
+            break
 
 if __name__ == '__main__': # 메인 엔트리
     print('프로그램 시작')
+    displayMenu()
     run()
 
 print('프로그램 종료')
